32de139f44fa5703a2c11c1dcbd1e9201b119185
phuonganh0804 <phuonganh0804@live.com>
2026-05-30 01:29:42 +0200

    feat: package Python dependencies as wheels and upload to S3

--- terraform/modules/extract_job/glue.tf
+++ terraform/modules/extract_job/glue.tf
@@ -12,10 +12,14 @@ resource "aws_glue_job" "extract_jobs" {
   }
 
   default_arguments = {
-    "--enable-job-insights" = "true"
-    "--job-language"        = "python"
-    "--bronze_bucket"       = var.bronze_bucket_name
-    "--ingest_date"         = var.ingest_date
+    "--enable-job-insights"        = "true"
+    "--job-language"               = "python"
+    "--bronze_bucket"              = var.bronze_bucket_name
+    "--ingest_date"                = var.ingest_date
+    "--additional-python-modules"  = join(",", [
+      for whl in fileset("${path.module}/../../assets/dependencies", "*.whl") :
+      "s3://${var.scripts_bucket_name}/assets/dependencies/${whl}"
+    ])
   }
 
   timeout           = each.value.timeout
@@ -27,6 +31,8 @@ resource "aws_glue_job" "extract_jobs" {
     Environment = var.environment
     Job         = each.key
   }
+
+  depends_on = [aws_s3_object.dependencies]
 }
 
 # Upload extract scripts to S3
@@ -37,6 +43,21 @@ resource "aws_s3_object" "extract_scripts" {
   key    = "assets/extract_jobs/${each.value.script}"
   source = "${path.module}/../../assets/extract_jobs/${each.value.script}"
   etag   = filemd5("${path.module}/../../assets/extract_jobs/${each.value.script}")
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
+
+# Upload all wheel files to S3
+resource "aws_s3_object" "dependencies" {
+  for_each = fileset("${path.module}/../../assets/dependencies", "*.whl")
+
+  bucket = var.scripts_bucket_name
+  key    = "assets/dependencies/${each.value}"
+  source = "${path.module}/../../assets/dependencies/${each.value}"
+  etag   = filemd5("${path.module}/../../assets/dependencies/${each.value}")
 
   tags = {
     Project     = var.project_name
